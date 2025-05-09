dfde94445deb9cdb21c9003af9c53b7a2c3b3f6d
Naveen Kumar <69457880+nvn-cgc@users.noreply.github.com>
2025-05-09 19:52:31 +0530

    Update load data.sql

--- scripts/bronze/load data.sql
+++ scripts/bronze/load data.sql
@@ -1,79 +1,68 @@
 /*
   ====================================================================================
-  Stored procedure: Load bronze layer(Source -> bronze)
+  SnowSQL: Load bronze layer(Source(locally stored) -> bronze)
   ====================================================================================
 
   scripts purposes: 
-    This stored procedure loads data from external CSV files into the 'bronze' schema.
+    It uploads (or "stages") local files from your computer to Snowflake to be loaded into tables later.
     It performs the following actions:
-      - Truncates the bronze tables before loading data.
-      - Uses the BULK INSERT command to load data from CSV files to bronze tables.
+      - We are using the PUT command to upload the data into stages
+		PUT file://<local_file_path> @<stage_name> [AUTO_COMPRESS = TRUE|FALSE] [OVERWRITE = TRUE|FALSE];
+      - We are using the COPY INTO command to copy data from the stage to the table in Snowflake
+		COPY INTO <table_name>
+		FROM <stage_location>
+		[ FILES = ('file1.csv', 'file2.csv') ]
+		[ FILE_FORMAT = (TYPE = 'CSV' [other options]) ]
+		[ ON_ERROR = 'CONTINUE' | 'SKIP_FILE' | 'SKIP_FILE_<num>' | 'ABORT_STATEMENT' ]
+		[ FORCE = TRUE | FALSE ];
+      - After loading data into a table, we can able to retrieve from the below-mentioned query
+		SELECT * FROM INFORMATION_SCHEMA.LOAD_HISTORY WHERE TABLE_NAME = 'CRM_CUST_INFO';
+
   ====================================================================================
 */
 
-CREATE OR ALTER PROCEDURE bronze.load_source AS
-BEGIN
-	PRINT 'DATA LOADED SUCCESSFULLY';
+--this needs to be run in SNOWSQL
+PUT file://F:/Program/MS_SQL_server/Project/sql_data_warehouse_project/datasets/source_crm/cust_info.csv @%crm_cust_info;
 
-	truncate table BRONZE.crm_cust_info;
-	
-	bulk insert BRONZE.crm_cust_info 
-	from 'F:\Program\MS SQL server\Project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
-	with(
-		firstrow = 2
-	   ,fieldterminator = ','
-	   ,tablock
-	);
-	
-	truncate table bronze.crm_prd_info;
-	
-	bulk insert  bronze.crm_prd_info
-	from 'F:\Program\MS SQL server\Project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
-	with(
-		firstrow = 2
-	   ,fieldterminator = ','
-	   ,tablock
-	);
-	
-	
-	truncate table bronze.crm_sales_details;
-	
-	bulk insert bronze.crm_sales_details 
-	from 'F:\Program\MS SQL server\Project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
-	with(
-		firstrow = 2
-	   ,fieldterminator = ','
-	   ,tablock
-	);
-	
-	truncate table bronze.erp_PX_CAT_G1V2;
-	bulk insert bronze.erp_PX_CAT_G1V2
-	from 'F:\Program\MS SQL server\Project\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
-	with(
-		firstrow = 2
-	   ,fieldterminator = ','
-	   ,tablock
-	);
-	
-	truncate table bronze.erp_CUST_AZ12;
-	
-	bulk insert bronze.erp_CUST_AZ12
-	from 'F:\Program\MS SQL server\Project\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
-	with(
-		firstrow = 2
-	   ,fieldterminator = ','
-	   ,tablock
-	);
-	
-	truncate table bronze.erp_LOC_A101;
-	
-	bulk insert bronze.erp_LOC_A101
-	from 'F:\Program\MS SQL server\Project\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
-	with(
-		firstrow = 2
-	   ,fieldterminator = ','
-	   ,tablock
-	);
-END;
+--this needs to be run in snowflake
+COPY INTO crm_cust_info
+FROM @%crm_cust_info
+FILE_FORMAT = (TYPE = 'CSV' SKIP_HEADER = 1 FIELD_OPTIONALLY_ENCLOSED_BY = '"');
 
-EXEC bronze.load_source;
+select * from crm_cust_info;
+
+PUT file://F:/Program/MS_SQL_server/Project/sql_data_warehouse_project/datasets/source_crm/prd_info.csv @%crm_prd_info;
+
+
+COPY INTO crm_prd_info
+from @%crm_prd_info
+file_format = (type = 'csv' skip_header = 1 field_optionally_enclosed_by = '"');
+
+select * from crm_prd_info;
+
+PUT file://F:/Program/MS_SQL_server/Project/sql_data_warehouse_project/datasets/source_crm/sales_details.csv @%crm_sales_details;
+
+Copy into crm_sales_details
+from @%crm_sales_details
+file_format = (type = 'csv' skip_header = 1 field_optionally_enclosed_by = '"');
+
+select * from crm_sales_details;
+
+PUT file://F:/Program/MS_SQL_server/Project/sql_data_warehouse_project/datasets/source_crm/cust_az12.csv @%erp_cust_az12;
+
+Copy into erp_cust_az12
+from @%erp_cust_az12
+file_format = (type = 'csv' skip_header = 1 field_optionally_enclosed_by = '"');
+
+PUT file://F:/Program/MS_SQL_server/Project/sql_data_warehouse_project/datasets/source_crm/loc_a101.csv @%erp_loc_a101;
+
+Copy into erp_loc_a101
+from @%erp_loc_a101
+file_format = (type = 'csv' skip_header = 1 field_optionally_enclosed_by = '"');
+
+PUT file://F:/Program/MS_SQL_server/Project/sql_data_warehouse_project/datasets/source_crm/px_cat_g1v2.csv @%erp_px_cat_g1v2;
+
+copy into erp_px_cat_g1v2
+from @%erp_px_cat_g1v2
+file_format = (type = 'csv' skip_header = 1 field_optionally_enclosed_by = '"');
+
